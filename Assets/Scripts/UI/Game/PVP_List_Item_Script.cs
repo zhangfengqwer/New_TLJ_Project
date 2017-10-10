e2d54c278e660baededce6860ae58952c7a259ef
hpzsr <582254172@qq.com>
2017-10-10 13:49:50 +0800

    主界面修改

---
+++ Assets/Scripts/UI/Game/PVP_List_Item_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PVP_List_Item_Script : MonoBehaviour {
+
+    public Text m_text_changci;
+    public Text m_text_kaisairenshu;
+    public Text m_text_baomingfei;
+    public Text m_text_baomingrenshu;
+
+    public Button m_button_baoming;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setChangCi(string str)
+    {
+        m_text_changci.text = str;
+    }
+
+    public void setKaiSaiRenShu(string str)
+    {
+        m_text_kaisairenshu.text = str;
+    }
+
+    public void setBaoMingFei(string str)
+    {
+        m_text_baomingfei.text = str;
+    }
+
+    public void setBaoMingRenShu(string str)
+    {
+        m_text_baomingrenshu.text = str;
+    }
+
+    public void onClickBaoMing()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
--- Assets/Scripts/UI/Main/MainScript.cs
+++ Assets/Scripts/UI/Main/MainScript.cs
@@ -3,11 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
+    public Button m_button_xiuxianchang;
+    public Button m_button_jingjichang;
+    public GameObject m_xiuxianchang;
+
+    // Use this for initialization
+    void Start ()
 	{
         AudioScript.getAudioScript().playMusic_GameBg();
     }
@@ -25,13 +30,38 @@ public class MainScript : MonoBehaviour {
     public void onClickEnterXiuXianChang()
     {
         AudioScript.getAudioScript().playSound_ButtonClick();
-        SceneManager.LoadScene("GameScene");
+        //SceneManager.LoadScene("GameScene");
+
+        m_button_xiuxianchang.transform.localScale = new Vector3(0,0,0);
+        m_button_jingjichang.transform.localScale = new Vector3(0,0,0);
+        m_xiuxianchang.transform.localScale = new Vector3(1, 1, 1);
+
+        m_xiuxianchang.GetComponent<Animation>().Play("xiuxianchang_show");
     }
 
     public void onClickEnterJingJiChang()
     {
         AudioScript.getAudioScript().playSound_ButtonClick();
+        //SceneManager.LoadScene("GameScene");
+
+        PVPChoiceScript.create();
+    }
+
+    public void onClickJingDianChang()
+    {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void onClickChaoDiChang()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void onClickXiuXianChang_back()
+    {
+        m_button_xiuxianchang.transform.localScale = new Vector3(1, 1, 1);
+        m_button_jingjichang.transform.localScale = new Vector3(1, 1, 1);
+        m_xiuxianchang.transform.localScale = new Vector3(0, 0, 0);
     }
 
     public void OnClickHead()
