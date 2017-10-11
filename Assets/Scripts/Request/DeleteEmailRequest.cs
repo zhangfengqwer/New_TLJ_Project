275fe7d113d38b0f05d7263583c5dca6f4142339
zhangfengqwer <984992820@qq.com>
2017-10-11 19:09:27 +0800

    1.增加删除邮件接口

---
+++ Assets/Scripts/Request/DeleteEmailRequest.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using TLJCommon;
+using UnityEngine;
+
+public class DeleteEmailRequest : Request
+{
+    private int emailId;
+    private bool flag;
+    private string result;
+
+    public void setEmailId(int id)
+    {
+        emailId = id;
+    }
+
+    private void Awake()
+    {
+        Tag = Consts.Tag_DeleteMail;
+    }
+
+    private void Update()
+    {
+        if (flag)
+        {
+            CallBack(result);
+            flag = false;
+        }
+    }
+    public delegate void ReadMailCallBack(string result);
+
+    public ReadMailCallBack CallBack;
+
+    public override void OnRequest()
+    {
+        JsonData jsonData = new JsonData();
+        jsonData["tag"] = Tag;
+        jsonData["uid"] = UserData.uid;
+        jsonData["email_id"] = emailId;
+        string requestData = jsonData.ToJson();
+        LogicEnginerScript.Instance.SendMyMessage(requestData);
+    }
+
+    public override void OnResponse(string data)
+    {
+        result = data;
+        flag = true;
+    }
+}
