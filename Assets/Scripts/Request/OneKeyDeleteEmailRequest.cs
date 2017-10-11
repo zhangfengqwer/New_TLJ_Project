58dc226fc9a4eae300bc86db29643db116af4e6f
zhangfengqwer <984992820@qq.com>
2017-10-11 19:35:02 +0800

    1.增加一键删除接口 2.增加一键领取接口

---
+++ Assets/Scripts/Request/OneKeyDeleteEmailRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using TLJCommon;
+using UnityEngine;
+
+public class OneKeyDeleteEmailRequest : Request
+{
+    private int emailId;
+    private bool flag;
+    private string result;
+
+    private void Awake()
+    {
+        Tag = Consts.Tag_OneKeyDeleteMail;
+    }
+
+    private void Update()
+    {
+        if (flag)
+        {
+            CallBack(result);
+            flag = false;
+        }
+    }
+    public delegate void ReadMailCallBack(string result);
+
+    public ReadMailCallBack CallBack;
+
+    public override void OnRequest()
+    {
+        JsonData jsonData = new JsonData();
+        jsonData["tag"] = Tag;
+        jsonData["uid"] = UserData.uid;
+        string requestData = jsonData.ToJson();
+        LogicEnginerScript.Instance.SendMyMessage(requestData);
+    }
+
+    public override void OnResponse(string data)
+    {
+        result = data;
+        flag = true;
+    }
+}
