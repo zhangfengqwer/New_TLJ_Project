c66ed0c69716691f98d45f0d612b3bfb2d54cf11
zhangfengqwer <984992820@qq.com>
2017-10-11 14:11:36 +0800

    1.增加获取邮箱接口

---
+++ Assets/Scripts/Request/GetEmailRequest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using TLJCommon;
+using UnityEngine;
+
+public class GetEmailRequest : Request {
+
+    private void Awake()
+    {
+        Tag = Consts.Tag_GetMail;
+    }
+
+    public override void OnRequest()
+    {
+        JsonData jsonData = new JsonData();
+        jsonData["tag"] = Tag;
+        jsonData["uid"] = UserData.uid;
+        string requestData = jsonData.ToJson();
+        LogicEnginerScript.Instance.SendMessage(requestData);
+    }
+
+    public override void OnResponse(string data)
+    {
+        JsonData jsonData = JsonMapper.ToObject(data);
+        var code = (int) jsonData["code"];
+        if (code == (int) Consts.Code.Code_OK)
+        {
+            LogicEnginerScript.IsSuccessList.Add(true);
+            UserMailData.getInstance().initJson(data);
+        }
+        else
+        {
+            ToastScript.createToast("返回邮箱数据错误");
+        }
+    }
+}
