ceb585cb9156f13ec4c59ec2bf2c117d7851f7e1
zhangfengqwer <984992820@qq.com>
2017-10-19 08:52:35 +0800

    1

---
+++ Assets/Scripts/Utils/DragUtil.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DragUtil : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler,IPointerDownHandler
+{
+    private RectTransform _rectTransform;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        _rectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    // begin dragging
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        SetDraggedPosition(eventData);
+      
+    }
+
+    // during dragging
+    public void OnDrag(PointerEventData eventData)
+    {
+        SetDraggedPosition(eventData);
+    }
+
+    // end dragging
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        SetDraggedPosition(eventData);
+    }
+
+    /// <summary>
+    /// set position of the dragged game object
+    /// </summary>
+    /// <param name="eventData"></param>
+    private void SetDraggedPosition(PointerEventData eventData)
+    {
+
+        // transform the screen point to world point int rectangle
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(_rectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos))
+        {
+            _rectTransform.position = globalMousePos - offset;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Vector3 globalMousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(_rectTransform, eventData.position, eventData.pressEventCamera, out globalMousePos);
+        offset = globalMousePos - _rectTransform.position;
+    }
+}
