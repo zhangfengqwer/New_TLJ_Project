8524e51a18df57de198bd3d212b4dded1bf2824f
zhangfengqwer <984992820@qq.com>
2017-10-24 12:56:40 +0800

    1.增加实名认证接口及UI

---
+++ Assets/Scripts/Request/RealNameRequest.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LitJson;
+using TLJCommon;
+using UnityEngine;
+
+public class RealNameRequest : Request {
+
+    private bool flag = false;
+    private string result;
+    private string realName;
+    private string identification;
+    private void Awake()
+    {
+        Tag = Consts.Tag_RealName;
+    }
+    void Update()
+    {
+        if (flag)
+        {
+            if (CallBack != null)
+            {
+                CallBack(result);
+            }
+            flag = false;
+        }
+    }
+    public delegate void RealNameCallBack(string result);
+
+    public RealNameCallBack CallBack = null;
+
+    public void OnRequest(string reaname, string identfy)
+    {
+        this.realName = reaname;
+        this.identification = identfy;
+        OnRequest();
+    }
+
+    public override void OnRequest()
+    {
+        JsonData jsonData = new JsonData();
+        jsonData["tag"] = Tag;
+        jsonData["uid"] = UserData.uid;
+        jsonData["realName"] = realName;
+        jsonData["identification"] = identification;
+        string requestData = jsonData.ToJson();
+        LogicEnginerScript.Instance.SendMyMessage(requestData);
+    }
+
+    public override void OnResponse(string data)
+    {
+        JsonData jsonData = JsonMapper.ToObject(data);
+        var code = (int)jsonData["code"];
+        if (code == (int)Consts.Code.Code_OK)
+        {
+            result = data;
+            flag = true;
+        }
+        else
+        {
+            Debug.Log("返回实名认证数据错误：" + code);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Game/RealNameScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RealNameScript : MonoBehaviour
+{
+    public InputField RealNameInputField;
+    public InputField IdentificationInputField;
+    private bool _isCorrectRealName;
+    private bool _isCorrectIdentification;
+    private string _realName;
+    private string _identification;
+    public static GameObject create()
+    {
+        GameObject prefab = Resources.Load("Prefabs/UI/Panel/RealNamePanel") as GameObject;
+        GameObject obj = GameObject.Instantiate(prefab, GameObject.Find("Canvas_Middle").transform);
+
+        return obj;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        RealNameInputField.onEndEdit.AddListener(delegate { GetRealName(RealNameInputField); });
+        IdentificationInputField.onEndEdit.AddListener(delegate { GetIdentification(IdentificationInputField); });
+    }
+
+    public void GetRealName(InputField input)
+    {
+        _isCorrectRealName = IDCardValidationUtil.CheckRealName(input.text);
+        if (_isCorrectRealName)
+        {
+            _realName = input.text;
+        }
+    }
+
+    public void GetIdentification(InputField input)
+    {
+        _isCorrectIdentification = IDCardValidationUtil.CheckIDCard(input.text);
+        if (_isCorrectIdentification)
+        {
+            _identification = input.text;
+        }
+    }
+
+    public void OnClickRealName()
+    {
+        if (_isCorrectRealName && _isCorrectIdentification)
+        {
+            LogicEnginerScript.Instance.GetComponent<RealNameRequest>().CallBack = realNameCallBack;
+            LogicEnginerScript.Instance.GetComponent<RealNameRequest>().OnRequest(_realName, _identification);
+        }
+        else
+        {
+            ToastScript.createToast("输入的信息有误");
+        }
+    }
+
+    private void realNameCallBack(string result)
+    {
+        print(result);
+    }
+}
