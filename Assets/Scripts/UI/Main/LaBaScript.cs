0f9e24855787d2ee4f66290c439d74b33aeeda29
hpzsr <582254172@qq.com>
2017-11-01 15:24:21 +0800

    11

--- Assets/Scripts/UI/Main/LaBaScript.cs
+++ Assets/Scripts/UI/Main/LaBaScript.cs
@@ -34,7 +34,16 @@ public class LaBaScript : MonoBehaviour {
         }
         else if(isEnd)
         {
-            m_text.text = "欢迎来到疯狂升级 点击喇叭可进行全服喊话哦~~";
+            isEnd = false;
+
+            if (RandomUtil.getRandom(1, 2) == 1)
+            {
+                m_text.text = "系统：欢迎来到疯狂升级 点击喇叭可进行全服喊话哦~~";
+            }
+            else
+            {
+                m_text.text = "系统：关注微信公众号：“星焱娱乐”，即送丰厚大礼噢~";
+            }
         }
 	}
 
