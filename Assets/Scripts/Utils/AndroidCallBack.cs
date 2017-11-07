2396f7df582c8cba84ab259ef7eed776fd385374
zhangfengqwer <984992820@qq.com>
2017-11-07 19:57:08 +0800

    1

--- Assets/Scripts/Utils/AndroidCallBack.cs
+++ Assets/Scripts/Utils/AndroidCallBack.cs
@@ -7,7 +7,9 @@ public class AndroidCallBack : MonoBehaviour {
 
     public delegate void onPauseCallBack();
     public static onPauseCallBack s_onPauseCallBack = null;
-
+    private bool isPause;
+    private bool isFocus;
+    
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -26,5 +28,24 @@ public class AndroidCallBack : MonoBehaviour {
     public void OnResumeCallBack(string data)
     {
         ToastScript.createToast("返回游戏:"+Thread.CurrentThread);
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        isPause = pauseStatus;
+        
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        isFocus = hasFocus;
+    }
+
+    private void Update()
+    {
+        if (isPause && !isFocus)
+        {
+            ToastScript.createToast("回到后台:"+Thread.CurrentThread);
+        }
     }
 }
