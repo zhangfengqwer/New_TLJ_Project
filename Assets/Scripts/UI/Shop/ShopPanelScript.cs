90a5ff2d160e4d2778f903f952d90fa640a94509
zf <984992820@qq.com>
2017-11-20 09:58:59 +0800

    1

--- Assets/Scripts/UI/Shop/ShopPanelScript.cs
+++ Assets/Scripts/UI/Shop/ShopPanelScript.cs
@@ -84,15 +84,14 @@ public class ShopPanelScript : MonoBehaviour
         int vipLevel = VipUtil.GetVipLevel(UserData.rechargeVip);
         int currentVipToTal = VipUtil.GetCurrentVipToTal(vipLevel);
         VipImage.sprite = Resources.Load<Sprite>("Sprites/Vip/shop_vip_" + vipLevel);
-//        Vip.text = "vip" + vipLevel;
-       
 
-        int left = currentVipToTal - UserData.rechargeVip;
+        var vipText = string.Format("累计充值" + "<color=#FF0000FF>{0}</color>" + ",即可升级到" + "<color=#FF0000FF>{1}</color>", currentVipToTal + "元", "VIP" + (vipLevel + 1));
+        if (vipLevel >= 10)
+        {
+            vipText = string.Format("<color=#FF0000FF>{0}</color>", "贵族等级已满");
+        }
+        VipExplain.text = vipText;
 
-        VipExplain.text =string.Format("累计充值" + "<color=#FF0000FF>{0}</color>" + ",即可升级到" + "<color=#FF0000FF>{1}</color>",
-            currentVipToTal+"元",
-            "VIP"+ (vipLevel + 1));
-            
         SlideText.text = UserData.rechargeVip + "/" + currentVipToTal;
         SliderVip.value = UserData.rechargeVip / (float) currentVipToTal;
     }
