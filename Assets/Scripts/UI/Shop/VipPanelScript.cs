bd688f3959c4c958f7da105a4da69090ce57fd8f
zf <984992820@qq.com>
2017-11-23 15:28:24 +0800

    1

--- Assets/Scripts/UI/Shop/VipPanelScript.cs
+++ Assets/Scripts/UI/Shop/VipPanelScript.cs
@@ -19,15 +19,18 @@ public class VipPanelScript : MonoBehaviour
 
     public Text WeeklyGoldNumText;
     public Text WeeklyDiamondNumText;
-    private List<VipData> vipDatas;
+    private static List<VipData> vipDatas;
 
     // Use this for initialization
     void Start()
     {
 
         InitVip();
-        LogUtil.Log(Resources.Load("VipRewardData").ToString());
-        vipDatas = LitJson.JsonMapper.ToObject<List<VipData>>(Resources.Load("VipRewardData").ToString());
+        if (vipDatas == null || vipDatas.Count == 0)
+        {
+            vipDatas = LitJson.JsonMapper.ToObject<List<VipData>>(Resources.Load("VipRewardData").ToString());
+        }
+       
 
         for (int i = 0; i < 10; i++)
         {
@@ -74,7 +77,7 @@ public class VipPanelScript : MonoBehaviour
         MyVipImage.sprite = Resources.Load<Sprite>("Sprites/Vip/shop_vip_" + vipLevel);
 
         var vipText = string.Format("累计充值" + "<color=#FF0000FF>{0}</color>" + ",即可升级到" + "<color=#FF0000FF>{1}</color>",
-            currentVipToTal + "元", "VIP" + (vipLevel + 1));
+            currentVipToTal + "元", "贵族" + (vipLevel + 1));
         if (vipLevel >= 10)
         {
             vipText = string.Format("<color=#FF0000FF>{0}</color>", "贵族等级已满");
