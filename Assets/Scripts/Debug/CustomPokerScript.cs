6b98b493f1c80b8cf5573eaab75e4419f0d6ee95
hpzsr <582254172@qq.com>
2017-12-12 12:39:29 +0800

    11

---
+++ Assets/Scripts/Debug/CustomPokerScript.cs
@@ -0,0 +1,36 @@
+﻿using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPokerScript : MonoBehaviour
+{
+    // 请求自定义牌型
+    public static void reqCustomPoker(string uid,List<TLJCommon.PokerInfo> pokerList)
+    {
+        if (pokerList.Count != 25)
+        {
+            ToastScript.createToast("张数必须为25");
+            return;
+        }
+
+        JsonData data = new JsonData();
+
+        data["tag"] = TLJCommon.Consts.Tag_DebugSetPoker;
+        data["uid"] = uid;
+
+        {
+            JsonData jarray = new JsonData();
+            for (int i = 0; i < pokerList.Count; i++)
+            {
+                JsonData jd = new JsonData();
+                jd["num"] = pokerList[i].m_num;
+                jd["pokerType"] = (int)pokerList[i].m_pokerType;
+                jarray.Add(jd);
+            }
+            data["pokerList"] = jarray;
+        }
+
+        PlayServiceSocket.s_instance.sendMessage(data.ToJson());
+    }
+}
