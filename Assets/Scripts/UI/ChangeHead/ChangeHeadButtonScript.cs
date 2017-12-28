bafa57c031b039e1e0b76bfdb2d43b00dd7b4bb9
hpzsr <582254172@qq.com>
2017-12-28 14:17:57 +0800

    11

--- Assets/Scripts/UI/ChangeHead/ChangeHeadButtonScript.cs
+++ Assets/Scripts/UI/ChangeHead/ChangeHeadButtonScript.cs
@@ -8,8 +8,25 @@ public class ChangeHeadButtonScript : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        gameObject.GetComponent<Image>().color = Color.gray;
-	}
+        List<string> list = new List<string>();
+        CommonUtil.splitStr(UserData.head, list, '_');
+
+        if (list.Count == 2)
+        {
+            int myCurHead = int.Parse(list[1]);
+            ChangeHeadPanelScript.s_instance.m_choiceHead = myCurHead;
+
+            for (int i = 0; i < 18; i++)
+            {
+                if (gameObject.transform.parent.Find((i + 1).ToString()).name == myCurHead.ToString())
+                {
+                    gameObject.transform.parent.GetChild(i).Find("Image").localScale = new Vector3(1, 1, 1);
+
+                    return;
+                }
+            }
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,13 +37,13 @@ public class ChangeHeadButtonScript : MonoBehaviour {
     {
         ChangeHeadPanelScript.s_instance.m_choiceHead = int.Parse(gameObject.transform.name);
 
-        gameObject.GetComponent<Image>().color = Color.white;
+        gameObject.transform.Find("Image").localScale = new Vector3(1,1,1);
 
-        for (int i = 0; i < 16; i++)
+        for (int i = 0; i < 18; i++)
         {
             if (gameObject.transform.parent.Find((i + 1).ToString()).name != gameObject.transform.name)
             {
-                gameObject.transform.parent.GetChild(i).GetComponent<Image>().color = Color.gray;
+                gameObject.transform.parent.GetChild(i).Find("Image").localScale = new Vector3(0,0,0);
             }
         }
     }
