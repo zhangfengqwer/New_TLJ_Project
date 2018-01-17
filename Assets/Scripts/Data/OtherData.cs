2cc780f7844587b2cac0ad5d25fee08b618ec083
hpzsr <582254172@qq.com>
2018-01-17 13:38:19 +0800

    11

--- Assets/Scripts/Data/OtherData.cs
+++ Assets/Scripts/Data/OtherData.cs
@@ -20,7 +20,7 @@ public class OtherData
 
     public static bool s_isFromSetToLogin = false;
     public static bool s_isFirstOpenGame = true;
-    public static bool s_isTest = true;
+    public static bool s_isTest = false;
     public static bool s_canDebug = false;
     public static bool s_hasCheckSecondPSW = false;
     public static bool s_canRecharge = false; // 是否开放充值
