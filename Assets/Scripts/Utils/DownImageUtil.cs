57e812d2b4c4861036352dc787d545eb28834e9c
hpzsr <582254172@qq.com>
2018-03-07 13:29:06 +0800

    11

--- Assets/Scripts/Utils/DownImageUtil.cs
+++ Assets/Scripts/Utils/DownImageUtil.cs
@@ -6,16 +6,23 @@ using UnityEngine.UI;
 
 public class DownImageUtil : MonoBehaviour
 {
-    public Image image;
-    public string url;
+    public Image m_image;
+    public string m_url;
     public static Dictionary<string, Texture2D> Images = new Dictionary<string, Texture2D>();
 
     void Start()
     {
+    }
+
+    public void startDown(string url)
+    {
+        m_url = url;
+        m_image = gameObject.GetComponent<Image>();
+
         Texture2D texture;
-        if (Images.TryGetValue(url, out texture))
+        if (Images.TryGetValue(m_url, out texture))
         {
-            image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            m_image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
         }
         else
         {
@@ -25,7 +32,7 @@ public class DownImageUtil : MonoBehaviour
 
     IEnumerator GetImage()
     {
-        UnityWebRequest www = UnityWebRequest.GetTexture(url);
+        UnityWebRequest www = UnityWebRequest.GetTexture(m_url);
         yield return www.Send();
 
         if (www.isError)
@@ -35,12 +42,12 @@ public class DownImageUtil : MonoBehaviour
         else
         {
             Texture2D texture = ((DownloadHandlerTexture) www.downloadHandler).texture;
-            if (!Images.ContainsKey(url))
+            if (!Images.ContainsKey(m_url))
             {
-                Images.Add(url, texture);
+                Images.Add(m_url, texture);
             }
 
-            image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            m_image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
         }
     }
 }
