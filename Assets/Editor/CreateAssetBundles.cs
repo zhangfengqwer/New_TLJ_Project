6db8b07ee35121d16adab0129efb4b82cb777f9d
hpzsr <582254172@qq.com>
2018-03-09 15:07:37 +0800

    11

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+#if UNITY_ANDROID
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
+#endif
+
+#if UNITY_IPHONE
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
+#endif
+
+#if UNITY_STANDALONE_WIN
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+#endif
+
+    }
+}
