becefc2a0260b61cafc6ea5750e3b5db46c17f78
zf <984992820@qq.com>
2018-03-13 17:48:24 +0800

    1

--- Assets/Editor/CreateAssetBundles.cs
+++ Assets/Editor/CreateAssetBundles.cs
@@ -13,14 +13,17 @@ public class CreateAssetBundles
         }
 
 #if UNITY_ANDROID
+        assetBundleDirectory += "/android";
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.Android);
 #endif
 
 #if UNITY_IPHONE
+        assetBundleDirectory += "/ios";
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.iOS);
 #endif
 
 #if UNITY_STANDALONE_WIN
+        assetBundleDirectory += "/pc";
         BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 #endif
 
